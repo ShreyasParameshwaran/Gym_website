e8d2d9feb28efcd48cf89fd3872a60195da37c27
Shivam77607 <shivamsharma77607@gmail.com>
2024-03-01 19:18:13 +0530

    refactor: improve code redability

--- gym website/responsive-gym-website/assets/js/main.js
+++ gym website/responsive-gym-website/assets/js/main.js
@@ -1,136 +1,118 @@
 /* =================SHOW MENU ================= */
-const navMenu= document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-if(navToggle){
-    navToggle.addEventListener('click',() =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
- }
-
-
-
-
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /* ================= CHANGE BACKGROUND HEADER  ================= */
-const scrollHeader =() => {
-
-    const header = document.getElementById('header')
-    this.scrollY >= 50  ? header.classList.add('bg-header')
-                        : header.classList.remove('bg-header')
-}
-window.addEventListener('scroll', scrollHeader)
-
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-const scrollactive = () =>{
-    const scrollY = window.pageYOffset
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-            
-              if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-                sectionClass.classList.add('active-link')
-              }else{
-                sectionClass.classList.remove('active-link')
-              }
-    })
-}
-window.addEventListener('scroll', scrollactive )
+const scrollactive = () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
 
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollactive);
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 const sr = ScrollReveal({
-    origin : 'top',
-    distance: '60px',
-    duration: 2500,
-    delay:400,
-})
-sr.reveal(`.home__data, .footer__container, .footer__group`)
-sr.reveal(`.home__img`,{delay:700, origin: 'bottom'})
-sr.reveal(`.logos__img, .program__card, .pricing__card`,{interval: 100})
-sr.reveal(`.choose__img, .calculate__content`,{origin: 'left'})
-sr.reveal(`.choose__content, .calculate__img`,{origin: 'right'})
-
-
-
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+sr.reveal(`.home__data, .footer__container, .footer__group`);
+sr.reveal(`.home__img`, { delay: 700, origin: "bottom" });
+sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 });
+sr.reveal(`.choose__img, .calculate__content`, { origin: "left" });
+sr.reveal(`.choose__content, .calculate__img`, { origin: "right" });
 
 /*=============== CALCULATE JS ===============*/
 
-
-
 const calculateBmi = () => {
-    const calculateForm = document.getElementById('calculate-form'),
-      calculateCm = document.getElementById('calculate-cm'),
-      calculateKg = document.getElementById('calculate-kg'),
-      calculateMessage = document.getElementById('calculate-message')
+  const calculateForm = document.getElementById("calculate-form"),
+    calculateCm = document.getElementById("calculate-cm"),
+    calculateKg = document.getElementById("calculate-kg"),
+    calculateMessage = document.getElementById("calculate-message");
 
-    if(calculateCm.value === ''  ||  calculateKg.value === ''){
-        calculateMessage.classList.remove('color-green')
-        calculateMessage.classList.add('color-red')
+  if (calculateCm.value === "" || calculateKg.value === "") {
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
 
-        calculateMessage.innerHTML = 'Fill in the Weight and Height'
-        setTimeout(() =>{
-            calculateMessage.textContent = ''
-        }, 3000)
-    }
-else{
-    setTimeout(() =>{
-        calculateMessage.textContent = ''
-    }, 3000)
+    calculateMessage.innerHTML = "Fill in the Weight and Height";
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
 
     //formula to calculate BMI
-    const cm = calculateCm.value/100
-    const kg = calculateKg.value
-    const bmi = Math.round(kg/(cm * cm))
+    const cm = calculateCm.value / 100;
+    const kg = calculateKg.value;
+    const bmi = Math.round(kg / (cm * cm));
 
+    // to show the health status of entered value
+    if (bmi < 18.5) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny`;
+    } else if (bmi < 25) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy`;
+    } else {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are overwweight`;
+    }
 
-          // to show the health status of entered value
-          if(bmi < 18.5){
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny`
-          } else if(bmi <25){
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy`
-          }
-          else{
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are overwweight`
-          }
+    //to clear the input field
+    calculateCm.value = "";
+    calculateKg.value = "";
 
-          //to clear the input field
-          calculateCm.value = ''
-          calculateKg.value = ''
+    //remove message in four seconds
 
-          //remove message in four seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 4000);
+  }
+};
 
-          setTimeout(() =>{
-            calculateMessage.textContent = ''
-          } ,4000)
-}
-}
+calculateForm.addEventListener("submit", calculateBmi);
 
-
-calculateForm.addEventListener('submit', calculateBmi)
-
-
-
-calculateForm.addEventListener('submit', calculateBmi)
+calculateForm.addEventListener("submit", calculateBmi);
 /*=============== EMAIL JS ===============*/
